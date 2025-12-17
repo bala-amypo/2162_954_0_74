d0f64102dcfde4328d8ca5dc181a959e513a4a4e
bala-amypo <bala@amypo.in>
2025-12-17 04:53:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/Studentcontroller.java
@@ -0,0 +1,7 @@
+package com.example.demo.Contoller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind
